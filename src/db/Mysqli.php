d8747bd449008c3ddd1ad81ff0c6ba8e3bc122c8
songran <821687825@qq.com>
2024-01-04 17:20:15 +0800

    up

---
+++ src/db/Mysqli.php
@@ -0,0 +1,238 @@
+<?php
+namespace gongju\db;
+date_default_timezone_set('Asia/Shanghai');
+
+class Mysqli {
+
+    /**
+     * MySQLi链接
+     */
+    private $link_id = null;
+    /**
+     * 数据库名
+     */
+    private $dbname;
+    /**
+     * 调试模式
+     */
+    private $debug = 1;
+    /**
+     * 构造函数
+     * 调用connect方法，返回$link_id
+     */
+    function __construct($dbhost, $dbuser, $dbpw, $dbname, $charset, $pconnect = 0) {
+        $this->link_id = $this->connect($dbhost, $dbuser, $dbpw, $dbname, $charset, $pconnect);
+    }
+    /**
+     * 打开一个MySql链接
+     * $dbhost  数据库地址
+     * $dbuse  数据库账号
+     * $dbpw  数据库密码
+     * $dbname  数据库名
+     * $pconnect 1:持久链接;0:普通链接
+     * $charset
+     * return $link_id
+     */
+    function connect($dbhost, $dbuser, $dbpw, $dbname = '', $charset = 'UTF8', $pconnect = 0) {
+        $func = $pconnect == 1 ? 'mysqli_pconnect' : 'mysqli_connect';
+        if (!$this->link_id = @$func($dbhost, $dbuser, $dbpw)) {
+            $this->message('Can not connect to MySQL server');
+            return false;
+        }
+        mysqli_query($this->link_id, "SET NAMES $charset");
+
+        if ($dbname && !@mysqli_select_db($this->link_id, $dbname)) {
+            $this->message('Cannot use database ' . $dbname);
+            return false;
+        }
+        $this->dbname = $dbname;
+        return $this->link_id;
+    }
+    /**
+     * 选择数据库
+     * return bool
+     */
+    function selectDb($dbname) {
+        if (!@mysqli_select_db($this->link_id, $dbname)) {
+            return false;
+        }
+
+        $this->dbname = $dbname;
+        return true;
+    }
+    /**
+     * 执行sql
+     */
+    function query($sql) {
+        $query = mysqli_query($this->link_id, $sql); //or $this->message('MySQL Query Error: ', $sql);
+        return $query;
+    }
+    /**
+     * @desc Select
+     *
+     * @param  $sql
+     * @param  $keyfield
+     * @return Result
+     */
+    function select($sql) {
+        $array  = [];
+        $result = $this->query($sql);
+        while ($r = $this->fetchArray($result)) {
+            $array[] = $r;
+        }
+        $this->freeResult($result);
+        return $array;
+    }
+
+    /**
+     * 获取第一条结果
+     */
+    function getfirst($sql) {
+        return $this->fetchArray($this->query($sql));
+    }
+
+    /**
+     * 执行插入sql
+     * $tablename  表名
+     * $array  需要插入的数据 array('filed=>$value)
+     * return bool
+     */
+    function insert($tablename, $array) {
+        return $this->query("INSERT INTO `$tablename`(`" . implode('`,`', array_keys($array)) . "`) VALUES('" . implode("','", $array) . "')");
+    }
+    /**
+     * 执行更新sql
+     * $tablename  表名
+     * $array  需要插入的数据  array('filed'=>$value)
+     * $where  条件
+     * @return bool
+     */
+    function update($tablename, $array, $where = '') {
+        if ($where) {
+            $sql = '';
+            foreach ($array as $k => $v) {
+                $sql .= ", `$k`='$v'";
+            }
+            $sql = substr($sql, 1);
+            $sql = "UPDATE `$tablename` SET $sql WHERE $where";
+        } else {
+            $sql = "REPLACE INTO `$tablename`(`" . implode('`,`', array_keys($array)) . "`) VALUES('" . implode("','", $array) . "')";
+        }
+        return $this->query($sql);
+    }
+    /**
+     * 获取主键
+     * $table 表名
+     * return Primary  filed
+     */
+    function getPrimary($table) {
+        $result = $this->query("SHOW COLUMNS FROM $table");
+        while ($r = $this->fetchArray($result)) {
+            if ($r['Key'] == 'PRI') {
+                break;
+            }
+
+        }
+        $this->freeResult($result);
+        return $r['Field'];
+    }
+
+    /**
+     * @desc  Get a record from database
+     *
+     * @param String $sql
+     * @param String $type    mysql_query  or   mysql_unbuffered_query
+     * @return   Result
+     */
+    function fetch($sql) {
+        $query = $this->query($sql);
+        $rs    = $this->fetchArray($query);
+        $this->freeResult($query);
+        return $rs;
+    }
+    /**
+     *  遍历结果集
+     *  $result_type MYSQL_ASSOC 关联索引;MYSQL_NUM 数字索引;MYSQL_BOTH 两者都有
+     */
+    function fetchArray($query, $result_type = MYSQLI_ASSOC) {
+        return mysqli_fetch_array($query, $result_type);
+    }
+    /**
+     * 从结果集中取得一行作为数组
+     */
+    function fetchRow($query) {
+        return mysqli_fetch_row($query);
+    }
+    /**
+     *  INSERT，UPDATE 或 DELETE 查询所影响的记录行数
+     */
+    function affectedRows() {
+        return mysqli_affected_rows($this->link_id);
+    }
+    /**
+     * 获取结果集行数
+     */
+    function numRows($result) {
+        return mysqli_num_rows($result);
+    }
+    /**
+     * 返回结果集中字段的数目
+     */
+    function numFields($result) {
+        return mysql_num_fields($result);
+    }
+
+    /**
+     * 释放所有与结果标识符 result 所关联的内存。
+     * return bool
+     */
+    function freeResult(&$result) {
+        return mysqli_free_result($result);
+    }
+    /**
+     * 获取上步insert产生的自增id
+     */
+    function insertId() {
+        return mysqli_insert_id($this->link_id);
+    }
+    /**
+     * 获取服务器版本
+     */
+    function version() {
+        return mysqli_get_server_info($this->link_id);
+    }
+    /**
+     * 关闭MySQL链接
+     * @return bool
+     */
+    function close() {
+        return mysqli_close($this->link_id);
+    }
+    /**
+     * 获取mysql错误信息
+     */
+    function error() {
+        return @mysqli_error($this->link_id);
+    }
+    /**
+     * 获取mysql错误号码
+     */
+    function errno() {
+        return intval(@mysqli_errno($this->link_id));
+    }
+    /**
+     * 错误信息处理
+     * $message  错误信息
+     * $sql  错误sql
+     */
+    function message($message = '', $sql = '') {
+        $this->errormsg = "<b>MySQL Query : </b>$sql <br /><b> MySQL Error : </b>" . $this->error() . " <br /> <b>MySQL Errno : </b>" . $this->errno() . " <br /><b> Message : </b> $message";
+        if ($this->debug) {
+            $msg = (defined('IN_ADMIN') || DEBUG) ? $this->errormsg : "Bad Request. $LANG[illegal_request_return]";
+            echo '<div style="font-size:12px;text-align:left; border:1px solid #9cc9e0; padding:1px 4px;color:#000000;font-family:Arial, Helvetica,sans-serif;"><span>' . $msg . '</span></div>';
+            exit;
+        }
+    }
+
+}
+?>
