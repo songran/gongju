ec480e1a09680bd6f12f952a79ebb3bb4d88d4e9
songran <821687825@qq.com>
2024-01-05 16:30:04 +0800

    redis

---
+++ test/redisclient/redis.php
@@ -0,0 +1,23 @@
+<?php
+
+include "./vendor/autoload.php";
+use \Gongju\Db\Redisclient;
+$config = [
+    'host'     => '192.168.1.110',
+    'port'     => '6379',
+    'auth'     => '',
+    'timeout'  => 60,
+    'pconnect' => 1,
+    'server'   => 0,
+];
+$redis = new Redisclient($config);
+
+//设置
+$res = $redis->set('log-master', 11111, 0, 0, $time = 10);
+
+var_dump($res);
+//获取
+$res = $redis->get('log-master');
+if ($res) {
+    echo 33333333;
+}
